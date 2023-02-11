0a75ee75fb3c49b3e19fd4c1017db9b817ed376c
Aaron Bertell <aaronbertell@Aarons-MacBook-Pro.local>
2023-02-10 21:50:23 -0800

    first commit

---
+++ src/Title/Title.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './titleStyles.css';
+
+function Title() {
+    return(
+        <div className='titleContainer'>
+            hello
+        </div>
+    );
+   
+}
+
+export default Title;
