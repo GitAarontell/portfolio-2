ea12c6051212517fa50bd4dc83b024c51e7b7cd7
GitAarontell <adbertell2@gmail.com>
2023-03-04 00:19:26 -0800

    added wind

--- src/SpaceObjects/Rocket.jsx
+++ src/SpaceObjects/Rocket.jsx
@@ -2,15 +2,30 @@ import React from 'react';
 import './rocketStyles.css';
 
 function Rocket({top, left}) {
+    // 1190 is what is used for css vw ratio
     return(
         <div className='rocketContainer' style={{top: `${top}`, left:`${left}`}}>
             <div className='rim'>
                 <div className='window'></div>
             </div>
             <div className='jetBurn'></div>
-            <div className='speedLines'></div>
+            <div className='speedLineContainer'>
+                <div className='speedLines'></div>
+                <div className='speedLines'></div>
+                <div className='speedLines'></div>
+                <div className='speedLines'></div>
+            </div>
         </div>
     );
 }
+/*
+    top: -20%;
+    right: 50%;
 
+    top: 0%;
+    right: -30%;
+
+    top: 60%;
+    right: -60%;
+*/
 export default Rocket;
--- src/SpaceObjects/Star.jsx
+++ src/SpaceObjects/Star.jsx
@@ -4,7 +4,7 @@ import './starstyles.css'
 function Star({top, left}) {
     return (
         <div className='starContainer' style={{top: `${top}`, left:`${left}`}}>
-            
+            <div className="star2"></div>
         </div>
     );
 }
